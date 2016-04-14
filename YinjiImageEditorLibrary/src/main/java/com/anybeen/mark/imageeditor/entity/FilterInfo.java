b73f93fa4fcd31e991c9a0db6773fc3838434db6
maidou <maidou@anybeen.net>
2016-04-14 10:56:13 +0800

    1、滤镜特效的 bean 类

---
+++ YinjiImageEditorLibrary/src/main/java/com/anybeen/mark/imageeditor/entity/FilterInfo.java
@@ -0,0 +1,51 @@
+package com.anybeen.mark.imageeditor.entity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by maidou on 2016/4/12.
+ */
+public class FilterInfo implements java.io.Serializable {
+    public int filterIndex = -1;        // 滤镜库索引（自定义唯一标识）
+    public String filterNameCh = "";    // 中文名称
+    public String filterNameEn = "";    // 英文名称
+    public int filterCount = 0;         // 操作次数
+
+    public JSONObject buildFilterInfoToJSONObj () throws JSONException {
+        JSONObject filterJson = new JSONObject();
+        try {
+            filterJson.put("filterIndex",   filterIndex);
+            filterJson.put("filterNameCh",  filterNameCh);
+            filterJson.put("filterNameEn",  filterNameEn);
+            filterJson.put("filterCount",   filterCount);
+        } catch (JSONException e) {
+            System.out.println("filter json build error");
+        }
+        return filterJson;
+    }
+
+    public void parseJSONObjToFilterInfo (JSONObject dataJson) {
+        try {
+            filterIndex     = dataJson.getInt("filterIndex");
+            filterNameCh    = dataJson.getString("filterNameCh");
+            filterNameEn    = dataJson.getString("filterNameEn");
+            filterCount     = dataJson.getInt("filterCount");
+        } catch (JSONException e) {
+            System.out.println("filter json parse error");
+        }
+    }
+
+    public void toStringFilterInfo() {
+        System.out.println(
+                "filterInfo[" +
+                        "filterIndex:" + filterIndex + "," +
+                        "filterNameCh:" + filterNameCh + "," +
+                        "filterNameEn:" + filterNameEn + "," +
+                        "filterCount:" + filterCount +
+                        "]"
+        );
+    }
+
+
+}
